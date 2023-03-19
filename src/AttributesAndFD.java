99f690adcb5b3b85ebb97dfe635d93ce1a2a2d88
Ayush-Asutkar <asutkarayush@gmail.com>
2023-03-20 01:30:09 +0530

    Completed BCNF synthesis

--- src/AttributesAndFD.java
+++ src/AttributesAndFD.java
@@ -72,15 +72,97 @@ public class AttributesAndFD {
                     // => currAttr - violatedFD.getRight()
                     // => violatedFD.getLeft() and violatedFD.getLeft()
 
+                    String firstStringToAdd = stringSubtraction(currAttr, violatedFD.getRight());
+                    String secondStringToAdd = stringUnion(violatedFD.getLeft(), violatedFD.getRight());
+
+                    ArrayList<FunctionalDependency> firstFD = getFDwrtAttr(firstStringToAdd, fds);
+                    ArrayList<FunctionalDependency> secondFD = getFDwrtAttr(secondStringToAdd, fds);
+
+//                    System.out.println("Violated fd = " + violatedFD);
+//                    System.out.println("firstStringToAdd = " + firstStringToAdd);
+//                    System.out.println("secondStringToAdd = " + secondStringToAdd);
+                    decomposition.put(firstStringToAdd, firstFD);
+                    decomposition.put(secondStringToAdd, secondFD);
+                    decomposition.remove(entry.getKey());
+
                     bcnf = false;
-
-
+                    break;
                 }
             }
         }
 
+        ArrayList<Attributes> result = new ArrayList<>();
+        for(String str: decomposition.keySet()) {
+            Attributes attr = new Attributes();
+            for(int i=0; i<str.length(); i++) {
+                attr.add(str.charAt(i));
+            }
+            result.add(attr);
+        }
 
-        return null;
+
+        return result;
+    }
+
+    //will return list of functional dependency which are valid for the given set of attributes
+    private ArrayList<FunctionalDependency> getFDwrtAttr(String attrString, ArrayList<FunctionalDependency> fds) {
+        Attributes attr = StringFunctionHelper.convertStringToAttributes(attrString);
+        ArrayList<FunctionalDependency> result = new ArrayList<>();
+
+        for(FunctionalDependency fd: fds) {
+            String left = fd.getLeft();
+            String right = fd.getRight();
+
+            boolean value = true;
+            for(int i=0; i<left.length(); i++) {
+                if(!attr.contains(left.charAt(i))) {
+                    value = false;
+                    break;
+                }
+            }
+
+            for(int i=0; i<right.length(); i++) {
+                if(!attr.contains(right.charAt(i))) {
+                    value = false;
+                    break;
+                }
+            }
+
+            if(value) {
+                result.add(fd);
+            }
+        }
+        return result;
+    }
+
+    //performs first - second
+    private String stringSubtraction(String first, String second) {
+        Attributes attr = StringFunctionHelper.convertStringToAttributes(second);
+        HashSet<Character> set = attr.getAttributes();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i=0; i<first.length(); i++) {
+            if(!set.contains(first.charAt(i))) {
+                stringBuilder.append(first.charAt(i));
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private String stringUnion (String first, String second) {
+        HashSet<Character> set = new HashSet<>();
+        char[] firstArray = first.toCharArray();
+        char[] secondArray = second.toCharArray();
+
+        for(char ch: firstArray) {
+            set.add(ch);
+        }
+
+        for(char ch: secondArray) {
+            set.add(ch);
+        }
+        return StringFunctionHelper.convertSetToString(set);
     }
 
     //returns null if given input is in bcnf,
@@ -147,176 +229,6 @@ public class AttributesAndFD {
         }
 
         return StringFunctionHelper.convertSetToString(result);
-    }
-
-    private String minLengthAttributeFinder(HashSet<String> attSet) {
-        String minSizeKey = null;
-        int minLength = Integer.MAX_VALUE;
-
-        for(String att: attSet) {
-            int attLength = att.length();
-
-            if(minLength > attLength) {
-                minSizeKey = att;
-                minLength = attLength;
-            }
-        }
-
-        return minSizeKey;
-    }
-
-    private HashSet<String> findSuperKey() {
-        HashSet<String> result = new HashSet<>();
-
-        String attrString = StringFunctionHelper.convertSetToString(attributes.getAttributes());
-        String sortedAttrString = StringFunctionHelper.sortString(attrString);
-        int length = sortedAttrString.length();
-
-        for(int i=0; i<(1<<length); i++) {
-            StringBuilder stringBuilder = new StringBuilder();
-            for(int j=0; j<length; j++) {
-                if(((i >> j) % 2 == 1)) {
-                    stringBuilder.append(sortedAttrString.charAt(j));
-                }
-            }
-            String currAttr = stringBuilder.toString();
-//            System.out.println("CurrAttr = " + currAttr);
-
-            String closureString = closure(currAttr, functionalDependencies);
-            String closureStringSorted = StringFunctionHelper.sortString(closureString);
-
-            if(sortedAttrString.equals(closureStringSorted)) {
-//                System.out.println("Super key = " + currAttr);
-                Attributes toBeAddedToSuperKey = new Attributes();
-                for(int k=0; k<currAttr.length(); k++) {
-                    toBeAddedToSuperKey.add(currAttr.charAt(k));
-                }
-                String resToAdd = StringFunctionHelper.convertSetToString(toBeAddedToSuperKey.getAttributes());
-                String sortedRes = StringFunctionHelper.sortString(resToAdd);
-                result.add(sortedRes);
-            }
-        }
-        return result;
-    }
-
-    private ArrayList<FunctionalDependency> findMinimalCover() {
-        /**
-         * Set minimal = functional dependencies*/
-        ArrayList<FunctionalDependency> minimal = new ArrayList<>();
-
-        /**
-         * Replace each functional dependency X->{A1,A2,A3,...An} in F by the n
-         ******** functional dependencies X->A1, X->A2, X->A3, ..., X->An*/
-        for (FunctionalDependency functionalDependency : functionalDependencies) {
-            String left = functionalDependency.getLeft();
-            String right = functionalDependency.getRight();
-
-            if (right.length() == 1) {
-                minimal.add(new FunctionalDependency(left, right));
-            } else {
-                char[] rightArr = right.toCharArray();
-                for (char ch : rightArr) {
-                    minimal.add(new FunctionalDependency(left, String.valueOf(ch)));
-                }
-            }
-        }
-
-//        System.out.println("After decomposing right side => " + minimal);
-        //working correctly till here
-
-
-        /**
-         * For each functional dependency X->A in F
-         ******* For each attribute B that is an element of X
-         **************** if {{F - {X -> A}} U {(X - {B}) -> A}} is equivalent to F
-         **************** then replace x->A with (X - {B}) -> A in F*/
-        for (int i=0; i<minimal.size(); i++) {
-            String left = minimal.get(i).getLeft();
-            String right = minimal.get(i).getRight();
-
-            if(left.length() == 1) {
-                continue;
-            }
-
-            for(int j=0; j<left.length(); j++) {
-                ArrayList<FunctionalDependency> mightBeMinimal = new ArrayList<>(minimal);
-
-                mightBeMinimal.remove(new FunctionalDependency(left, right));
-
-                StringBuilder newLeft = new StringBuilder();
-                for(int k=0; k<left.length(); k++) {
-                    if (k != j) {
-                        newLeft.append(left.charAt(k));
-                    }
-                }
-
-                mightBeMinimal.add(new FunctionalDependency(newLeft.toString(), right));
-
-                if(equivalenceCheckerFD(minimal, mightBeMinimal)) {
-                    //both are equivalent
-                    //remove the functional dependency and add the smaller one
-                    minimal.remove(new FunctionalDependency(left, right));
-                    minimal.add(new FunctionalDependency(newLeft.toString(), right));
-//                    System.out.println("i = " + i);
-                    i--;
-                    break;
-                }
-
-                mightBeMinimal.clear();
-            }
-        }
-
-//        System.out.println("After remove extraneous attribute from left side=> " + minimal);
-
-        for(int i=0; i<minimal.size(); i++) {
-            String left = minimal.get(i).getLeft();
-            String right = minimal.get(i).getRight();
-
-            ArrayList<FunctionalDependency> mightBeMinimal = new ArrayList<>(minimal);
-
-            mightBeMinimal.remove(new FunctionalDependency(left, right));
-
-            if(equivalenceCheckerFD(minimal, mightBeMinimal)) {
-                //remove the current functional dependency
-                minimal.remove(i);
-                i--;
-            }
-        }
-
-//        System.out.println("After removing redundant dependency => " + minimal);
-        return minimal;
-    }
-
-    private boolean equivalenceCheckerFD(ArrayList<FunctionalDependency> first, ArrayList<FunctionalDependency> second) {
-        /** Test first covers second*/
-        boolean firstCoversSecond = coverChecker(first, second);
-
-        /** Test second covers first*/
-        boolean secondCoversFirst = coverChecker(second, first);
-
-        return firstCoversSecond && secondCoversFirst;
-    }
-
-    private boolean coverChecker(ArrayList<FunctionalDependency> first, ArrayList<FunctionalDependency> second) {
-        /**
-         * Step - 1:- For each functional dependency, x->y in second calculate x+ with respect to second
-         * Step - 2:- For each functional dependency, x->y in second calculate x+ with respect to first
-         * If all attributes determined by step 1 is subset of all attributes determined by step 2
-         * => first covers second*/
-
-        for(FunctionalDependency fd : second) {
-            String left = fd.getLeft();
-            String right = fd.getRight();
-
-            String closureWRTSecond = closure(left, second);
-            String closureWRTFirst = closure(left, first);
-
-            if(!StringFunctionHelper.stringContainsCharacters(closureWRTFirst, closureWRTSecond)) {
-                return false;
-            }
-        }
-
-        return true;
     }
 
     @Override
